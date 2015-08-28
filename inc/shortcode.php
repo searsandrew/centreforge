e8b93704f1e030ff0fb1ef04818765e6b410ede5
Andrew Sears <searsandrew@gmail.com>
2015-08-28 11:24:16 -0500

    Added in shortcode backwards compatibility

--- inc/shortcode.php
+++ inc/shortcode.php
@@ -10,7 +10,8 @@ function cf_add_pdficon($atts, $content = null) {
 add_shortcode('pdf-icon', 'cf_add_pdficon');
 
 
-/* ** Since: Centreforge v2.0.2 ** */
+/* ** Since: Centreforge v2.0.2 
+ * Updated: Centreforge v2.1.8 ** */
 function cf_bs_columns($atts, $content = null) {
 	extract(shortcode_atts(array(
 		"col" => '12',
@@ -21,6 +22,7 @@ function cf_bs_columns($atts, $content = null) {
 	return ($row == 'first'?'<div class="row">':'').'<div class="col-'.$size.'-'.$col.' '.$class.'">' . do_shortcode($content) . '</div>'.($row == 'last'?'</div>':'');
 }
 add_shortcode('bs-columns', 'cf_bs_columns');
+add_shortcode('class', 'cf_bs_columns'); //Remove in Centreforge 3.0
 
 /* ** Since: Centreforge v2.1.3
  * Updated: Centreforge v2.1.4 ** */
@@ -60,13 +62,15 @@ function cf_bs_button($atts, $content = null) {
 		"url" => '#',
 		"class" => 'default',
 		"size" => 'md',
-		"style" => ''
+		"style" => '',
+		"version" => 'deprecated' //Remove in Centreforge 3.0
 		), $atts));
-	return '<a href="'.$url.'" class="btn btn-'.$class.' '.$size.'" style="'.$style.'">'.do_shortcode($content).'</a>';
+	return '<a href="'.$url.'" class="btn btn-'.($version == 'deprecated'?$class:$version).' '.(strpos($size,'btn-') === FALSE?'btn-':'').$size.'" style="'.$style.'">'.do_shortcode($content).'</a>';
 }
 add_shortcode('button', 'cf_bs_button');
 
-/* ** Since: Centreforge v2.1.4 ** */
+/* ** Since: Centreforge v2.1.4 
+ * Updated: Centreforge v2.1.8 ** */
 function cf_bs_modal($atts, $content = null){
 	extract(shortcode_atts(array(
         "button" => 'Click to Open',
@@ -75,9 +79,13 @@ function cf_bs_modal($atts, $content = null){
         "btncloseclass" => 'default',
         "footer" => '',
         "modalsize" => '',
-        "modalid" => '1'
+        "modalid" => 'modal-1',
+        "class" => 'deprecated', //Remove in Centreforge 3.0
+        "closebtn" => 'deprecated', //Remove in Centreforge 3.0
+        "size" => 'deprecated', //Remove in Centreforge 3.0 
+        "id" => 'deprecated' //Remove in Centreforge 3.0
     ), $atts, 'modal'));
-	return '<button type="button" class="btn btn-'.$btnopenclass.'" data-toggle="modal" data-target="#'.$modalid.'">'.$button.'</button><div id="'.$modalid.'" class="modal fade"><div class="modal-dialog '.$modalsize.'"><div class="modal-content">'.($title != ''?'<div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button><h4 class="modal-title">'.$title.'</h4></div>':'').'<div class="modal-body">'.do_shortcode($content).'</div>'.($footer != ''?'<div class="modal-footer">'.$footer.'<button type="button" class="btn btn-'.$btncloseclass.'" data-dismiss="modal">Close</button></div>':'').'</div></div></div>';
+	return '<button type="button" class="btn btn-'.($class == 'deprecated'?$btnopenclass:$class).'" data-toggle="modal" data-target="#'.($id == 'deprecated'?$modalid:'modal-'.$id).'">'.$button.'</button><div id="'.($id == 'deprecated'?$modalid:'modal-'.$id).'" class="modal fade"><div class="modal-dialog '.($size == 'deprecated'?$modalsize:'modal-'.$size).'"><div class="modal-content">'.($title != ''?'<div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button><h4 class="modal-title">'.$title.'</h4></div>':'').'<div class="modal-body">'.do_shortcode($content).'</div>'.($footer != ''?'<div class="modal-footer">'.$footer.'<button type="button" class="btn btn-'.($closebtn == 'deprecated'?$btncloseclass:$closebtn).'" data-dismiss="modal">Close</button></div>':'').'</div></div></div>';
     
 }
 add_shortcode('modal','cf_bs_modal');
