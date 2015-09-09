d71a15ba109e2888185bb9b3b19c83b34ae5f8b9
Michael Read <michaelread1208@gmail.com>
2015-09-09 16:01:49 -0500

    Updated the 404 page.

--- 404.php
+++ 404.php
@@ -1,3 +1,14 @@
 <?php get_header(); ?>
-
+    <div id="primary" class="site-content col-md-<?= (is_active_sidebar( 'sidebar-page' )) ? '8' : '12'; ?>">
+        <div id="content" role="main">
+            <header class="page-header">
+                <h1>404 - Page not found</h1>
+            </header><!-- .page-header -->
+            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                <p>Sorry, the page you were looking for could not be found.  Please use the navigation links above to get back on track or try searching.</p>
+                <?php get_search_form(); ?>
+            </article>
+        </div><!-- #content -->
+    </div><!-- #primary .site-content -->
+    <?php get_sidebar(); ?>
 <?php get_footer(); ?>
