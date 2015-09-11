6e7a6485c3aa01918fe1f57ba12096af9bd05b72
Michael Read <michaelread1208@gmail.com>
2015-09-11 11:03:26 -0500

    Updated / Removed the remaining template files that we needed to include into the theme.

--- index.php
+++ index.php
@@ -7,7 +7,23 @@
                 <?php get_template_part( 'content', 'blog' ); ?>
 
             <?php endwhile; // end of the loop. ?>
+            
+            <?php $paginationLinks = paginate_links( array(
+                'current'   => max( 1, get_query_var('paged') ),
+                'total'     => $wp_query->max_num_pages,
+                'next_text' => 'Older Posts &rarr;',
+                'prev_text' => '&larr; Newer Posts',
+                'type'      => 'array'
+            )); 
 
+            if(!empty($paginationLinks > 0)){
+                echo '<ul class="pager">';
+                foreach($paginationLinks as $link){
+                     echo '<li>'.$link.'</li>';   
+                }
+                echo '</ul>';
+            } ?>
+            
         </div><!-- #content -->
     </div><!-- #primary .site-content -->
 
--- search.php
+++ search.php
@@ -4,18 +4,31 @@
 			<?php if ( have_posts() ) : ?>
 
 				<header class="page-header">
-					<?php
-						the_archive_title( '<h1 class="page-title">', '</h1>' );
-						the_archive_description( '<div class="taxonomy-description">', '</div>' );
-					?>
-				</header><!-- .page-header -->
+                    <h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'twentyfifteen' ), get_search_query() ); ?></h1>
+                </header><!-- .page-header -->
 
 	            <?php while ( have_posts() ) : the_post(); ?>
 	
 	                <?php get_template_part( 'content', 'blog' ); ?>
 	
 	            <?php endwhile; // end of the loop. ?>
-				
+            
+				<?php $paginationLinks = paginate_links( array(
+                    'current'   => max( 1, get_query_var('paged') ),
+                    'total'     => $wp_query->max_num_pages,
+                    'next_text' => 'Older Posts &rarr;',
+                    'prev_text' => '&larr; Newer Posts',
+                    'type'      => 'array'
+                )); 
+
+                if(!empty($paginationLinks > 0)){
+                    echo '<ul class="pager">';
+                    foreach($paginationLinks as $link){
+                         echo '<li>'.$link.'</li>';   
+                    }
+                    echo '</ul>';
+                } ?>
+            
 			<?php endif; ?>
 				
         </div><!-- #content -->
