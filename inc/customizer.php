de64e09b62186dba5260120406fbed64535bcc6b
Michael Read <michaelread1208@gmail.com>
2015-11-02 14:38:09 -0600

    Added in a dropdown menu on the theme customizer that shows all of the different nav-***.php template parts for both Centreforge and any child theme nav template parts.

--- inc/customizer.php
+++ inc/customizer.php
@@ -49,6 +49,45 @@ function centreforge_customize_register($wp_customize){
  	)));
 	
     // Menu Options
+    
+    $cf_nav_types = array();
+    
+    // Find all files in the centreforge theme with nav-
+    $cfcore_nav_types = glob(TEMPLATEPATH."/nav-*.php");
+    // Set the array to blank in case we don't need this
+    $cfchild_nav_types = array();
+    
+    // If there is a child theme preset, get the child theme navs
+    if(TEMPLATEPATH != STYLESHEETPATH){
+        //Find all Child Theme nav-
+        $cfchild_nav_types = glob(STYLESHEETPATH."/nav-*.php");
+    }
+    
+    $cf_nav_types = array_merge($cfcore_nav_types, $cfchild_nav_types);
+    
+    
+    if((count($cf_nav_types) > 0)) {
+        $menuTypes = array();
+        foreach($cf_nav_types as $cf_nav_type) {
+            $info = pathinfo($cf_nav_type);
+            $filename = str_replace('nav', '', $info['filename']);
+            $filename = str_replace('-', ' ', $filename);
+            $menuTypes[$info['filename']] = ucwords($filename);
+        }
+        
+        $wp_customize->add_setting('cf_options[menu_type]', array(
+            'capability' => 'edit_theme_options',
+            'type' => 'option'
+        ));
+        $wp_customize->add_control('cf_options[menu_type]', array(
+            'label'    => 'Primary Menu Style',
+            'section'  => 'nav',
+            'settings' => 'cf_options[menu_type]',
+            'type' => 'select',
+            'choices' => $menuTypes
+        ));
+    }
+    
     $wp_customize->add_setting('cf_menu_options[show_menu]', array(
 		'capability' => 'edit_theme_options',
 		'type' => 'option',
@@ -56,7 +95,7 @@ function centreforge_customize_register($wp_customize){
 	));
 	$wp_customize->add_control('cf_menu_options[show_menu]', array(
 	 	'settings' => 'cf_menu_options[show_menu]',
-        'label'    => 'Show Menu',
+        'label'    => 'Show Main Menu',
 	 	'section' => 'nav',
 		'type' => 'checkbox',
  	));
@@ -131,53 +170,10 @@ function centreforge_customize_register($wp_customize){
         'default' => '#337ab7',
         'label' => 'Content Link Color'
     );
-    $colors[] = array(
-        'slug'=>'cf_colors[bs_primary_color]', 
-        'default' => '#337ab7',
-        'label' => 'Primary Button Color'
-    );
-    $colors[] = array(
-        'slug'=>'cf_colors[bs_success_color]', 
-        'default' => '#5cb85c',
-        'label' => 'Success Button Color'
-    );
-    $colors[] = array(
-        'slug'=>'cf_colors[bs_info_color]', 
-        'default' => '#46b8da',
-        'label' => 'Info Button Color'
-    );
-    $colors[] = array(
-        'slug'=>'cf_colors[bs_warning_color]', 
-        'default' => '#f0ad4e',
-        'label' => 'Warning Button Color'
-    );
-    $colors[] = array(
-        'slug'=>'cf_colors[bs_danger_color]', 
-        'default' => '#d9534f',
-        'label' => 'Danger Button Color'
-    );
-    foreach( $colors as $color ) {
-        // SETTINGS
-        $wp_customize->add_setting(
-            $color['slug'], array(
-                'default' => $color['default'],
-                'type' => 'option', 
-                'capability' => 'edit_theme_options'
-            )
-        );
-        // CONTROLS
-        $wp_customize->add_control(
-            new WP_Customize_Color_Control(
-                $wp_customize,
-                $color['slug'], 
-                array('label' => $color['label'], 
-                'section' => 'colors',
-                'settings' => $color['slug'])
-            )
-        );
-    }
 }
 add_action('customize_register','centreforge_customize_register');
+
+
 
 // Get all the social profiles in the customizer.  Also has the option to grab just one of the social media profiles
 function cfcustomizer_get_social_profiles($which = 'all'){
